3d73edd3e7f53f4f9d5d0691be358f8e6d729961
geneus003 <geneus003@gmail.com>
2019-04-03 21:13:08 +0300

    Playing music in channel

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def play_music(song_name, clientik):
 
 def main():
 
-    token = ""
+    token = ""   # Discord bot token
 
-    client = discord.Client()
+    client = discord.Client()   # Discord client
 
     @client.event
-    async def on_ready():
+    async def on_ready():   # To show starting of the work and some information
 
         print('Logged in as')
         print(client.user.name)
@@ -28,15 +28,18 @@ def main():
         if message.author == client.user:   # Just need it
             return
 
-        if message.content == "!gc!creator":    # Info about author
+        if message.content == "!gc!creator":
 
-            await message.channel.send("My creator is Geneus003")
-            await message.channel.send("Email: geneus003@gmail.com")
-            await message.channel.send("Qiwi: +79832095427")    # Info about author# Info about author
+            # Info about author
+
+            creator_message_text = "My creator is Geneus003 \n Email: geneus003@gmail.com"
+
+            await client.send_message(message.channel, creator_message_text)
 
         if message.content == "!gc!help":   # Info about commands
 
-            await message.channel.send("Some instructions")
+            await client.send_message(message.channel, "Some instructions")
+            await client.send_message(message.channel, "Some more instuctions")
 
         if message.content == "!gc!list":   # Send ID and names of songs
 
@@ -52,19 +55,24 @@ def main():
                 list_of_music_list += str(counter_for_music_list) + ") " + str(i[:-4]) + "\n"
                 counter_for_music_list += 1
 
-            await message.channel.send(list_of_music_list)
+            await client.send_message(message.channel, list_of_music_list)
 
         if message.content == "!gc!play":
 
-            default_folder = "/home/geneus/Projects/Discord_bots/DiscordBot_GachiMusic/songs"
-            music_folder = default_folder + "Fairy_Tail_main_theme.mp3"
+            default_folder = "/home/geneus/Projects/Discord_bots/DiscordBot_GachiMusic/songs"   # Path to all music
+            music_folder = default_folder + "/Fairy_Tail_main_theme.mp3"    # Path to correct music
 
+            print("Starting playing music")
             # music_thread = Thread(target=play_music(music_folder, client))
             # music_thread.start()
 
-            channel = client.get_channel("425248908172853254")
+            channel = client.get_channel("425248908172853254")      # Id of voice channel
 
-            client.join_voice_channel(channel)
+            bot_voice = await client.join_voice_channel(channel)       # Joining to voice channel
+
+            music_player = bot_voice.create_ffmpeg_player(music_folder)     # Creating player
+
+            music_player.start()    # Playing music in voice channel
 
     client.run(token)
 
