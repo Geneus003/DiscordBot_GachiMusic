b3d58c7ad08c2b9c0feb50198604ce0b4c5f6253
geneus003 <geneus003@gmail.com>
2019-04-22 00:51:41 +0300

    Preparing to 1.1 version

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
 import os
-import random
 import math
+import asyncio
+import random
 
 
 class BotInformation:
@@ -17,10 +18,12 @@ def get_list_of_local_songs():
 
     # Function to get all local song's names
 
-    default_folder = "/home/geneus/Projects/Discord_bots/DiscordBot_GachiMusic"  # Path to project folder
+    default_folder = "."  # Path to project folder
 
     folder_with_songs = default_folder + "/songs"  # Path to songs folder
     folder_with_songs = os.listdir(folder_with_songs)  # List of songs names
+
+    folder_with_songs.sort()
 
     return folder_with_songs
 
@@ -36,12 +39,33 @@ def main():
 
     token = ""      # Discord bot token
 
-    status_game = discord.Game("Try !gc!help | V 1.02")     # Variable to set status
+    status_game = discord.Game("Try !gc!help | V 1.1")     # Variable to set status
     client = discord.Client(activity=status_game)       # Discord Client
 
     servers_list = []       # List with BotInformation classes
 
     delete_server_info_file()   # Deleting file with id of servers
+
+    async def timing_tasks_of_discord():
+        await client.wait_until_ready()
+
+        while not client.is_closed():
+
+            for i_temp in servers_list:
+
+                if i_temp.bot_voice is None:
+
+                    continue
+
+                if len(i_temp.bot_voice.channel.members) == 1:
+
+                    await i_temp.bot_voice.disconnect()
+                    i_temp.bot_voice = None
+                    i_temp.music_pl = None
+
+            await asyncio.sleep(120)
+
+    client.loop.create_task(timing_tasks_of_discord())
 
     @client.event
     async def on_ready():
@@ -54,6 +78,14 @@ def main():
         print(client.user.name)
         print(client.user.id)
         print('------')
+
+    """
+    @client.event
+    async def on_member_update():
+
+        print("wow")
+    
+    """
 
     @client.event
     async def on_message(message):
@@ -107,38 +139,62 @@ def main():
 
             parsed_message = message.content.split(" ")
 
+            if len(parsed_message) == 1:
+
+                parsed_message.append("1")
+
             list_of_local_songs = get_list_of_local_songs()
 
-            if len(parsed_message) > 2:
-                await message.channel.send("Incorrect input")
-                return
+            if parsed_message[1].isdigit():
 
-            if len(parsed_message) == 1:
-                parsed_message.append("1")
-                start_point_to_show_list = 0
-            else:
-                if not parsed_message[1].isdigit():
+                if len(parsed_message) > 2:
                     await message.channel.send("Incorrect input")
                     return
-                if int(parsed_message[1]) > math.ceil(len(list_of_local_songs) / 5) or int(parsed_message[1]) <= 0:
-                    await message.channel.send("Not enough songs, do number smaller or bigger")
-                    return
 
-                start_point_to_show_list = 5 * (int(parsed_message[1]) - 1)
+                if len(parsed_message) == 1:
+                    parsed_message.append("1")
+                    start_point_to_show_list = 0
 
-            local_songs_message = ""
+                else:
+                    if not parsed_message[1].isdigit():
+                        await message.channel.send("Incorrect input")
+                        return
 
-            if start_point_to_show_list + 5 > len(list_of_local_songs):
-                start_point_to_show_list -= start_point_to_show_list + 5 - len(list_of_local_songs)
+                    if int(parsed_message[1]) > math.ceil(len(list_of_local_songs) / 5) or int(parsed_message[1]) <= 0:
+                        await message.channel.send("Not enough songs, do number smaller or bigger")
+                        return
 
-            for i in range(start_point_to_show_list, start_point_to_show_list + 5):
+                    start_point_to_show_list = 5 * (int(parsed_message[1]) - 1)
 
-                local_songs_message += str(i + 1) + ") " + list_of_local_songs[i][:-4] + "\n"
+                local_songs_message = ""
 
-            local_songs_message += "List number " + parsed_message[1]
-            local_songs_message += " from " + str(math.ceil(len(list_of_local_songs) / 5))
+                if start_point_to_show_list + 5 > len(list_of_local_songs):
+                    start_point_to_show_list -= start_point_to_show_list + 5 - len(list_of_local_songs)
 
-            await message.channel.send(local_songs_message)
+                for i in range(start_point_to_show_list, start_point_to_show_list + 5):
+                    local_songs_message += str(i + 1) + ") " + list_of_local_songs[i][:-4] + "\n"
+
+                local_songs_message += "List number " + parsed_message[1]
+                local_songs_message += " from " + str(math.ceil(len(list_of_local_songs) / 5))
+
+                await message.channel.send(local_songs_message)
+
+            else:
+
+                local_songs_message = ""
+                needful_song_name = ""
+
+                for i in range(1, len(parsed_message)):
+
+                    needful_song_name += parsed_message[i].upper()
+
+                for i in range(len(list_of_local_songs)):
+
+                    if needful_song_name in list_of_local_songs[i].upper():
+
+                        local_songs_message += str(i+1) + ") " + list_of_local_songs[i] + "\n"
+
+                await message.channel.send(local_songs_message)
 
         if message.content.startswith("!gc!play"):
 
@@ -240,32 +296,49 @@ def main():
             bot_information.music_pl = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(road_to_correct_song))
             bot_information.bot_voice.play(bot_information.music_pl)
 
+        if message.content.startswith("!gc!yt"):
+
+            if bot_information.bot_voice is None:
+
+                bot_information.bot_voice = await message.author.voice.channel.connect()
+
+            else:
+
+                if bot_information.bot_voice.channel.id != message.author.voice.channel.id:
+
+                    await bot_information.bot_voice.disconnect()
+
+                    bot_information.bot_voice = await message.author.voice.channel.connect()
+
+            if bot_information.bot_voice is not None:
+
+                if bot_information.bot_voice.is_playing():
+                    bot_information.bot_voice.stop()
+
+            print(discord.opus)
+
+        if message.content == "!gc!memes":
+
+            default_folder = "."
+
+            additional_folder = default_folder + "/Pictures/memes/"
+
+            list_of_local_memes = os.listdir(additional_folder)
+
+            meme_image = additional_folder + list_of_local_memes[random.randint(0, len(list_of_local_memes) - 1)]
+
+            await message.channel.send(file=discord.File(meme_image))
+
         if message.content.upper() == "ПОЧЕМУ РОТ В ВОЛОДЕ" or message.content.upper() == "ПОЧЕМУ РОТ В ВОЛОДЕ?":
 
             # Function which send mem
 
             default_folder = "."
 
-            additional_folder = default_folder + "/Pictures/mort.jpg"
+            additional_folder = default_folder + "/Pictures/another/mort.jpg"
 
             await message.channel.send("Миша, что он на меня орет, да и у меня глобал на основе, а не у него")
             await message.channel.send(file=discord.File(additional_folder))
-
-        if message.content == "!gc!secret_0000" and message.author.id == 565938590031282186:
-
-            # print("here")
-
-            for i_temp in servers_list:
-
-                if i_temp.bot_voice is None:
-
-                    continue
-
-                if len(i_temp.bot_voice.channel.members) == 1:
-
-                    await i_temp.bot_voice.disconnect()
-                    i_temp.bot_voice = None
-                    i_temp.music_pl = None
 
     client.run(token)
 
